0a6c80e3d670864b5310f22c3bdd73b7aa14d9a8
Vuong V. Trinh <vanvuong.trinh@gmail.com>
2019-06-25 11:53:28 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def index():
     return render_template('index.html')
  
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='0.0.0.0')
+    app.run(debug=True, use_reloader=True)
